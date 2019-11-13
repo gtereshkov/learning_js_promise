6f3205cf06fb67b0964c8599a16037bb97a19461
kempelBLR <36518375+kempelBLR@users.noreply.github.com>
2019-11-13 22:48:47 +0300

    uploading js promise attempts

---
+++ js/service.js
@@ -0,0 +1,6 @@
+function getImages(pageNumber){
+    const promise = axios.get(`https://repetitora.net/api/JS/Images?page=${pageNumber}&count=1`);
+    return promise.then((response) => {
+        return response.data;
+    })
+};
